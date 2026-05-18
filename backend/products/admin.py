2dfe2ca503b6f2cdd6ec2de28b789c46f6bb0015
brenda <brenda@synetechworld.com>
2026-05-18 10:39:26 +0200

    registrar productos en admin y gestionarlos desde ahí

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "product_type",
+        "cabinet_width_mm",
+        "cabinet_height_mm",
+        "pixel_pitch",
+        "price",
+        "is_active",
+    ]
+    list_filter = ["product_type", "is_active"]
+    search_fields = ["name"]
