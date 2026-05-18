df96a4ef03904dfcf8559913f579699e4d425395
brenda <brenda@synetechworld.com>
2026-05-18 10:45:19 +0200

    endpoints de lectura

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+'''ReadOnlyModelViewSet
+↓
+crea endpoints de lectura
+↓
+GET lista
+↓
+GET detalle'''
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.filter(is_active=True).order_by("name")
+    serializer_class = ProductSerializer
